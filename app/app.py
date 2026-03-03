827e6d6629df0ce2861d0de5afe9cc9243cf53be
torturado <73028283+torturado@users.noreply.github.com>
2026-03-03 09:12:23 +0100

    afegir les pàgines en les que trabeballarem

--- app/app.py
+++ app/app.py
@@ -8,5 +8,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+	return render_template('login.html')
 
+@app.route('/register')
+def register():
+	return render_template('register.html')
 
+@app.route('/pong')
+def pong():
+	return render_template('pong.html')
+
+@app.route('/trexpres')
+def trexpres():
+	return render_template('trexpres.html')
+
+@app.route('/atencioflash')
+def atencioflash():
+	return render_template('atencioflash.html')
