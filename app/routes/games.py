1122f07d32d4675f22268e09d206ae4f9aa5fb8c
torturado <73028283+torturado@users.noreply.github.com>
2026-03-12 14:13:47 +0100

    les pàgines de jocs no podran detectar el username sense passar-li la sessió abans

--- app/routes/games.py
+++ app/routes/games.py
@@ -1,19 +1,15 @@
-from flask import render_template, Blueprint, request, jsonify
-from app.models.game import Game
-from app.models.game_session import GameSession
+from flask import render_template, Blueprint, session
 
 games_bp = Blueprint('games', __name__, url_prefix='/games')
 
 @games_bp.route('/pong')
 def pong():
-	return render_template('pong.html')
+	return render_template('pong.html', username=session.get('username'))
 
 @games_bp.route('/trexpres')
 def trexpres():
-	return render_template('trexpres.html')
+	return render_template('trexpres.html', username=session.get('username'))
 
 @games_bp.route('/atencioflash')
 def atencioflash():
-	return render_template('atencioflash.html')
-
-
+	return render_template('atencioflash.html', username=session.get('username'))
