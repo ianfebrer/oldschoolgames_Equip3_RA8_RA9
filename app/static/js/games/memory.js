02875ac8834dc1554de52643177cd7344d34e004
miguelfonollosa-pixel <miguelfonollosa@iesmontsia.org>
2026-04-05 13:42:14 +0200

    Agregar imatges al index i fer funcionar els botons del joc de memoria

--- app/static/js/games/memory.js
+++ app/static/js/games/memory.js
@@ -1,6 +1,4 @@
 const icon = ['🍎', '🍌', '🍇', '🍓', '🍒', '🍍', '🥝', '🍋'];
-//Esta variable el que fa es dublicar la varibale de dalt per a que siguen 16 cartes
-let cards = icon.concat(icon);
 const gameboard = document.getElementById('board');
 
 //Esta funcio el que fa es barrejar la llista "cards", creant una variable "j" que amb
@@ -12,93 +10,121 @@ function barrejar(llista) {
         [llista[i], llista[j]] = [llista[j], llista[i]];
     }
 }
-barrejar(cards);
 
+function initGame() {
+    gameboard.innerHTML = '';
+    let cards = icon.concat(icon);
+    barrejar(cards);
 
-cards.forEach(icon => {
-    // Creem una variable que el que fa es crear els div al html
-    const card = document.createElement('div');
+    cards.forEach(iconVal => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.value = iconVal;
+        card.addEventListener('click', flipCard);
+        gameboard.appendChild(card);
+    });
 
-    // Afegirem la variable card a la clase card del CSS, per a que agafo els estils
-    card.classList.add('card');
+    score = 0;
+    updateScore();
+}
 
-    // Aqui li guardem cada element de la llista a la carta.
-    card.dataset.value = icon;
+function bindControls() {
+    const startButton = document.getElementById("start-game");
+    const pauseButton = document.getElementById("pause-game");
+    const endButton = document.getElementById("end-game");
 
-    //Esta funcio el que fara es donarli la volta a la carta quan fesem click
-    card.addEventListener('click', flipCard);
+    if (startButton) startButton.addEventListener("click", startGame);
+    if (pauseButton) pauseButton.addEventListener("click", pauseGame);
+    if (endButton) endButton.addEventListener("click", endGame);
+}
 
-    // Aqui afegirem la variable card dins del tablero que hem creat al html.
-    gameboard.appendChild(card);
-});
+function startGame() {
+    if (gameState === "running") return;
 
-//Crearem estes variables que ens ajudaran a fer la següent funcio.
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+    // Si estem aturats o al final, inicialitzem cartes
+    if (gameState === "idle" || gameState === "finished") {
+        initGame();
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    } else if (gameState === "paused") {
+        // En reiniciar des de pausa, només desbloquegem targetes
+        lockBoard = false;
+    }
 
+    gameState = "running";
+}
+
+function pauseGame() {
+    if (gameState !== "running") return;
+    gameState = "paused";
+    lockBoard = true; // Bloquegem el taulell
+}
+
+function endGame() {
+    if (gameState === "idle") return;
+    gameState = "finished";
+    lockBoard = true;
+    gameboard.innerHTML = "";
+    score = 0;
+    updateScore();
+}
+
+function updateScore() {
+    const scoreElement = document.getElementById("player1-score");
+    if (scoreElement) {
+        scoreElement.textContent = "La teua puntuació: " + score;
+    }
+}
 
 function flipCard() {
-    // Aqui si el tablero esta block o fem doble click no fara res
     if (lockBoard) return;
     if (this === firstCard) return;
 
-    //Li afegim clase del css
     this.classList.add('flipped');
-    //Guardem a la variable les icones
-    this.textContent = this.dataset.value
+    this.textContent = this.dataset.value;
 
-    //Aqui comprovarem si la primera carta s'ha pegat la volta i quina
     if (!hasFlippedCard) {
-        hasFlippedCard = true
-        firstCard = this
+        hasFlippedCard = true;
+        firstCard = this;
         return;
     }
-    // Una vegada a pegat la volta la primera li diem que la segona tmb
+
     secondCard = this;
-    // I a esta funcio comprovarem si son iguals o no
     checkForMatch();
-
 }
 
-// Esta es la funcio anterior que comprovem
 function checkForMatch() {
-    //Aqui creem una variable que guarda un true o false. Si les dos cartes son iguals sera true
-    // si no sera false
     let isMatch = firstCard.dataset.value === secondCard.dataset.value;
-    //I aqui si isMatch es true, fara la funcio disableCards, si no fara l'altra
-    isMatch ? disableCards() : unflipCards();
-
+    if (isMatch) {
+        score += 10;
+        updateScore();
+        disableCards();
+    } else {
+        unflipCards();
+    }
 }
 
-//Esta funcio nomes se fara si les dos cartes son iguals
 function disableCards() {
-    //Li borrem el css de quan estaven boca avall
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-    //I fem un reset del tablero en esta funcio
     resetBoard();
 }
 
-//Esta funcio nomes se executa si son diferents
 function unflipCards() {
     lockBoard = true;
-    //Dixem 1 segon pa que puguesem vore les cartes i despues li peguem la volta
     setTimeout(() => {
-        //Borrem el css de quan estaven boca a dalt
         firstCard.classList.remove('flipped');
         secondCard.classList.remove('flipped');
-
-        //Tornem a tindre un valor buit
         firstCard.textContent = '';
         secondCard.textContent = '';
-
         resetBoard();
     }, 1000);
 }
 
-//I esta funcio fa un reset del tablero tan com si es guanya o perd
 function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
+    // Si estem en pausa, assegurem que el taulell segueixi bloquejat després d'amagar la carta incorrecta
+    [hasFlippedCard, lockBoard] = [false, gameState !== "running"];
     [firstCard, secondCard] = [null, null];
 }
+
+bindControls();
